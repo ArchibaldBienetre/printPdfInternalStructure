4b8e59d7e4bcc294e93c6c114fa6c9f5826a6dc1
Robert G <bluewhobert@mailbox.org>
2019-08-06 21:07:06 +0200

    simple PDF content output

--- src/main/java/org/example/PdfStructurePrinter.java
+++ src/main/java/org/example/PdfStructurePrinter.java
@@ -1,10 +1,43 @@
 package org.example;
 
+import com.lowagie.text.pdf.PdfDictionary;
+import com.lowagie.text.pdf.PdfName;
+import com.lowagie.text.pdf.PdfObject;
+import com.lowagie.text.pdf.PdfReader;
+
 import java.io.PrintStream;
 
 class PdfStructurePrinter {
 
     void print(String filePath, PrintStream out) throws Exception {
-        // implement me
+        PdfReader reader = new PdfReader(filePath);
+
+        out.println("### ROOT ###");
+        PdfDictionary root = reader.getCatalog();
+        printRecursively(reader, root, out, "");
+
+        out.println();
+        out.println("### Pages ###");
+        int pageCount = reader.getNumberOfPages();
+        for (int pageNumber = 1; pageNumber <= pageCount; pageNumber++) {
+            out.println();
+            out.println("## Page " + pageNumber + " ##");
+            PdfDictionary page = reader.getPageN(pageNumber);
+            printRecursively(reader, page, out, "");
+        }
+    }
+
+    private void printRecursively(PdfReader reader, PdfDictionary dictionary, PrintStream out, String prefix) {
+        for (Object keyObject : dictionary.getKeys()) {
+            PdfName key = (PdfName) keyObject;
+            PdfObject value = dictionary.get(key);
+            String outputFirstPart = prefix + key.toString() + ": (" + value.getClass().getSimpleName() + "] ";
+            if (value.isDictionary()) {
+                out.println(outputFirstPart);
+                printRecursively(reader, dictionary.getAsDict(key), out, prefix + "\t");
+            } else {
+                out.println(outputFirstPart + value.toString());
+            }
+        }
     }
 }
